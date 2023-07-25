096af6b87c4637714847e8504846d01590145a20
laurab23 <122219584+laurab23@users.noreply.github.com>
2023-07-25 19:46:49 +0200

    fin

--- app/public/wp-content/themes/kadence-child/functions.php
+++ app/public/wp-content/themes/kadence-child/functions.php
@@ -1,5 +1,5 @@
 <?php 
-/* Child theme generated with WPS Child Theme Generator */
+/* theme enfant genéré */
             
 if ( ! function_exists( 'b7ectg_theme_enqueue_styles' ) ) {            
     add_action( 'wp_enqueue_scripts', 'b7ectg_theme_enqueue_styles' );
@@ -9,3 +9,20 @@ if ( ! function_exists( 'b7ectg_theme_enqueue_styles' ) ) {
         wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
     }
 }
+//chargemt stylecss dans le child
+function styleplanty() {
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
+    
+    }
+    add_action( 'wp_enqueue_scripts', 'styleplanty');
+    // END ENQUEUE PARENT ACTION
+    
+//hook admin
+function link_admin($items, $args) {
+    if( is_user_logged_in() && $args->theme_location == 'primary' ){
+    $items .= '<li><a href="http://localhost/planty/wp-admin" class="admin">Admin</a></li>'; 
+    }
+
+  return $items;
+}
+add_filter('wp_nav_menu_items', 'link_admin', 10, 2);
